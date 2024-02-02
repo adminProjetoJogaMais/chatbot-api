33fa86fb023ff5ed7371ba98a442e3fa4677bb9f
adminProjetoJogaMais <157061973+adminProjetoJogaMais@users.noreply.github.com>
2024-02-01 21:50:16 -0300

    Add files via upload

---
+++ AuthenticationController.js
@@ -0,0 +1,53 @@
+const { compare } = require('bcrypt');
+const { sign, verify } = require('jsonwebtoken');
+const Usuario = require('../models/Usuario');
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const availableUsers = ['Datax Dev'];
+
+        const user = await Usuario.findOne({
+            where: { nome: username },
+        });
+
+        if (!user || !await compare(password, user.senha) || !availableUsers.includes(user.nome)) return res.status(401).json({
+            status: 'failed',
+            message: 'authentication failed'
+        });
+
+        const accessToken = sign({ name: user.nome }, process.env.SECRET, { expiresIn: '1d' });
+        res.json({ accessToken });
+    } catch (e) {
+        return res.status(401).json({
+            status: 'failed',
+            message: 'authentication failed'
+        });
+    }
+}
+
+exports.authenticate = async (req, res, next) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (!token) return res.status(401).json({
+            status: 'failed',
+            message: 'authentication failed'
+        });
+
+        verify(token, process.env.SECRET, (err, user) => {
+            if (err) return res.status(401).json({
+                status: 'failed',
+                message: 'authentication failed'
+            });
+
+            next();
+        })
+    } catch (e) {
+        return res.status(401).json({
+            status: 'failed',
+            message: 'authentication failed'
+        });
+    }
+}
